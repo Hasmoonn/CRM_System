529a541a092aafca93e2bd221ec6af8031798bd1
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-05-09 08:17:34 +0530

    server.js modified for deployment

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -14,7 +14,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://crm-system-frontend-five.vercel.app"  
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
